624a599ac4b89975c8bbe71dbe0347a2dae5cd51
Ashryz <tarek.ashry9894@outlook.com>
2024-03-05 14:12:04 +0200

    first task

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "ToDo App",
+    'summary': """ """,
+    'description': """ """,
+    'author': "Tarek Ashry",
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data':[
+        # 'security/ir.model.access.csv'
+        'views/base_menu.xml',
+        'views/ticket.xml'
+
+    ]
+
+}
